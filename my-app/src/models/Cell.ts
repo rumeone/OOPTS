5e9d442977f7611339cfb6f148b2a7ee036afd3c
rumeone <yuaroslav_29@mail.ru>
2022-06-19 02:13:46 +0700

    add board to page

--- my-app/src/models/Cell.ts
+++ my-app/src/models/Cell.ts
@@ -1,3 +1,25 @@
+import {Colors} from "./Colors";
+import {Figure} from "./figures/Figure";
+import {Board} from "./Board";
+
+
 export class Cell {
+    readonly x: number;
+    readonly y: number;
+    readonly color: Colors;
+    figure: Figure | null;
+    board: Board;
+    available: boolean;
+    id: number;
+
+    constructor(board: Board, x: number, y: number, color: Colors, figures: Figure | null) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+        this.figure = figures;
+        this.board = board;
+        this.available = false;
+        this.id = Math.random();
+    }
 
 }
