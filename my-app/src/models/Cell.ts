ec74533f588532ebf8008f688397d83502d89865
rumeone <yuaroslav_29@mail.ru>
2022-06-20 19:16:53 +0700

    added move method

--- my-app/src/models/Cell.ts
+++ my-app/src/models/Cell.ts
@@ -22,4 +22,12 @@ export class Cell {
         this.id = Math.random();
     }
 
+    moveFigure(target: Cell) {
+        if(this.figure && this.figure?.canMove(target)) {
+            this.figure?.moveFigure(target);
+            target.figure = this.figure;
+            this.figure = null;
+        }
+    }
 }
+
