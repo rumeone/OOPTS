cb85329d5cbf69de70904456e41c17733794cb93
rumeone <yuaroslav_29@mail.ru>
2022-06-22 00:43:59 +0700

    Add move method and check empty cell

--- my-app/src/components/CellComponent.tsx
+++ my-app/src/components/CellComponent.tsx
@@ -11,7 +11,7 @@ const CellComponent: FC<CellProps> = ({cell, selected, click}) => {
     return (
         <div className ={["cell", cell.color, selected ? "selected" : ""].join(" ")}
         onClick={() => click(cell)}
-        style={{background: cell.available && !cell.figure ? 'green' : ''}}
+        style={{background: cell.available && cell.figure ? 'green' : ''}}
         >
             {cell.available &&!cell.figure && <div className={"available"}/>}
             {cell.figure?.logo && <img src={cell.figure.logo} alt=""/>}
